e7b399bc7821a9ac3b468e99bcd9b731873ffa74
ShadowChild <shadychild12@gmail.com>
2020-11-08 17:08:19 +0000

    Updated profile selection!

--- src/main/java/uk/co/innoxium/baldursgate/BaldursGateModule.java
+++ src/main/java/uk/co/innoxium/baldursgate/BaldursGateModule.java
@@ -3,7 +3,7 @@ package uk.co.innoxium.baldursgate;
 import uk.co.innoxium.candor.module.AbstractModInstaller;
 import uk.co.innoxium.candor.module.AbstractModule;
 import uk.co.innoxium.candor.module.RunConfig;
-import uk.co.innoxium.candor.util.NativeDialogs;
+import uk.co.innoxium.candor.util.WindowUtils;
 
 import javax.swing.filechooser.FileSystemView;
 import java.io.File;
@@ -14,7 +14,6 @@ public class BaldursGateModule extends AbstractModule {
 
     public BaldursGateModule() {
 
-        BG3Settings.init();
     }
 
     private File game;
@@ -39,18 +38,25 @@ public class BaldursGateModule extends AbstractModule {
     @Override
     public void setGame(File file) {
 
+        BG3Settings.init();
+
         this.game = file;
         // Game home is BaldursGate3, game is /bin/bg3.exe
         this.gameHome = file.getParentFile().getParentFile();
         if(!loaded && BG3Settings.playerProfile.isEmpty()) {
 
-            NativeDialogs.showInfoDialog("Baldur's Gate 3 Module",
-                    "Please select the player profile to use\nClick ok to contrinue.",
-                    "ok",
-                    "info",
-                    true);
-            File playerProfile = NativeDialogs.openPickFolder(playerProfiles);
-            BG3Settings.playerProfile = playerProfile.getAbsolutePath();
+            PlayerProfileSelector profileSelector = new PlayerProfileSelector(playerProfiles);
+            profileSelector.setVisible(true);
+            WindowUtils.mainFrame.setFocusable(false);
+
+//            NativeDialogs.showInfoDialog("Baldur's Gate 3 Module",
+//                    "Please select the player profile to use\nClick ok to continue.",
+//                    "ok",
+//                    "info",
+//                    true);
+//            File playerProfile = NativeDialogs.openPickFolder(playerProfiles);
+
+//                BG3Settings.playerProfile = playerProfile.getAbsolutePath();
             loaded = true;
         }
     }

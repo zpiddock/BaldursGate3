23bf2041843ea3f6ab87e169edfa78698109fa33
Zach Piddock <zpiddock@gmail.com>
2021-04-13 20:04:37 +0100

    Add new menus Non Functional currently

--- src/main/java/uk/co/innoxium/baldursgate/BaldursGateModule.java
+++ src/main/java/uk/co/innoxium/baldursgate/BaldursGateModule.java
@@ -4,10 +4,14 @@ import uk.co.innoxium.candor.game.Game;
 import uk.co.innoxium.candor.module.AbstractModInstaller;
 import uk.co.innoxium.candor.module.AbstractModule;
 import uk.co.innoxium.candor.module.RunConfig;
+import uk.co.innoxium.candor.util.Logger;
 import uk.co.innoxium.candor.util.WindowUtils;
+import uk.co.innoxium.candor.window.dialog.SwingDialogs;
 
+import javax.swing.*;
 import javax.swing.filechooser.FileSystemView;
 import java.io.File;
+import java.util.ArrayList;
 
 
 public class BaldursGateModule extends AbstractModule {
@@ -160,5 +164,51 @@ public class BaldursGateModule extends AbstractModule {
                 return new File(getGame().getParentFile().getParentFile(), "Launcher").getAbsolutePath();
             }
         };
+    }
+
+    @Override
+    public ArrayList<JMenuItem> getMenuItems() {
+
+        ArrayList<JMenuItem> list = new ArrayList<>();
+        list.add(new InstallModFixer());
+        list.add(new LSLib());
+
+        return list;
+    }
+
+    public static class InstallModFixer extends JMenuItem {
+
+        public InstallModFixer() {
+
+            this.setText("Install Mod Fixer");
+            this.addActionListener(e -> {
+
+                if(SwingDialogs.showConfirmDialog("BG3 Module", "Are you sure you wish to install the bg3 mod fixer?", JOptionPane.QUESTION_MESSAGE, JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
+
+                    Logger.info("Attempting to download and install mod fixer");
+                    // in the future we may use the Nexus API to find the latest version
+                }
+            });
+        }
+    }
+
+    public static class LSLib extends JMenuItem {
+
+        public LSLib() {
+
+            this.setText("Install/Update LSLib by Norbyte");
+            this.setToolTipText("LSLib super-powers the Baldur's Gate 3 Module");
+            this.addActionListener(e -> {
+
+                // Steps
+                // Show confirm dialog
+                // Check if already installed
+                //      if yes - check if update
+                //              if yes - download update
+                //              if no - inform user LSLib is up to date
+                //      if no - download latest version
+                // Extract downloaded archive to <candor-path>\tools\bg3\lslib
+            });
+        }
     }
 }
